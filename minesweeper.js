67eb7b40d7995ba530fa50eaa0e5e6cd82ec807f
tony-luisi <t.luisi@gmail.com>
2016-06-20 10:02:36 +1200

    add right click

--- minesweeper.js
+++ minesweeper.js
@@ -10,8 +10,14 @@ function startGame () {
 
 function addListeners(cell) {
   cell.addEventListener('click', showCell)
+  cell.addEventListener('contextmenu', markCell)
 }
 
 function showCell(evt) {
   evt.target.classList.remove('hidden')
+}
+
+function markCell(evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('marked')
 }
