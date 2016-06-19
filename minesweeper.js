cbd71e4875588b922dba4d80731c9b8d6bb52134
tony-luisi <t.luisi@gmail.com>
2016-06-20 09:44:06 +1200

    shows cells when clicked

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  var board = document.getElementsByClassName('board')
+  var children = board[0].children
+  for (var i = 0; i < children.length; i++){
+    addListeners(children[i])
+  }
+}
+
+function addListeners(cell) {
+  cell.addEventListener('click', showCell)
+}
+
+function showCell(evt) {
+  evt.target.classList.remove('hidden')
 }
